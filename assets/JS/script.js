06bd7b15d9f2aa077a6d19446b147c2130fbc57e
Shamsddine <goumdissshamsddine@gmail.com>
2023-11-06 16:02:32 +0100

    bloucle

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -16,7 +16,7 @@ class character {
             this.nom = _nom;
             this.life=_vie;
             this.attack = _attaque;
-            this.defenses = _defense;
+            this.defense = _defense;
             this.existe = true;
         } else {
             this.nom = "error";
@@ -26,22 +26,67 @@ class character {
 
     }
     info(){
-        console.log(this.nom + ": vie:" + this.life + "- attaque:" + this.attack);
+        console.log(this.nom + ": vie:" + this.life + "- attaque:" + this.attack + "-defense:" + this.defense );
+    }
+    attaquer(_defenseur) {
+        console.log("attaque de" + this.nom + "sur" + _defenseur.nom)
+
+        if (this.attaque > _defenseur.defense) {
+            _defenseur.life -= 10
+
+        }
+        else if (this.attaque == _defenseur.defense) {
+
+            _defenseur.life -= 5
+        }
+        else {
+            this.life -= 5
+        }
+
+        if (this.life <= 0) {
+            console.error(this.nom + "est mort")
+            this.existe = false
+        }
+
+        else if (_defenseur.vie <= 0) {
+            console.error(_defenseur.nom + "est mort")
+            this.existe = false
+        }
     }
 }
 
 
 
-let perso1 = new character("");
-let perso2 = new character("Achab", genereChiffre(10,50), genereChiffre(10,50),genereChiffre(10,50));
-let perso3 = new character("");
-let perso4 = new character("Dazai Osamu", genereChiffre(10,50),genereChiffre(10,50),genereChiffre(10,50));
+// let perso1 = new character("Achab", genereChiffre(10,50), genereChiffre(10,50),genereChiffre(10,50));
+// let perso2 = new character("Dazai Osamu", genereChiffre(10,50),genereChiffre(10,50),genereChiffre(10,50));
 
-console.log(perso1.existe,perso1.nom);
-console.log(perso3.existe,perso3.nom);
-console.log(perso2);
-console.log(perso4);
-console.log("Nouveau personnage", perso4.nom);
+perso1.info()
 
 perso2.info()
-perso4.info()
+
+perso1.attaquer(perso2)
+
+perso1.info()
+
+perso2.info()
+
+const nbrJoueur = 2
+
+players = []
+
+fot (let i = 0; i < nbrJoueur; i ++){
+    const nom = prompt("Veuillez entrer un nom")
+    const vie = genereChiffre(10,50)
+    const attaque = genereChiffre(10,50)
+    const def = genereChiffre(10,50)
+
+    const player = new character (nom, vie, attaque, def)
+    player.info()
+
+    players.push(player)
+
+}
+
+function run(){
+
+}
